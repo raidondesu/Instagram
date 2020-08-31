b0f1404b19c2bd3520f716f49352445353d0115f
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-31 13:56:19 -0400

    Placed material UI avatar into header

--- instagram/src/Post.js
+++ instagram/src/Post.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Post.css";
-
+import Avatar from "@material-ui/core/Avatar";
 function Post() {
   return (
-    <div>
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="RafehQazi"
+          src="/static/images/avatar/1.jpg"
+        />
+        {/* header, avatar + username */}
+      </div>
       <h3>Username</h3>
-      {/* header, avatar + username */}
       <img
         className="post__image"
         src="https://venturebeat.com/wp-content/uploads/2015/01/Microsoft-sign-campus.jpg?fit=4160%2C2340&strip=all"
