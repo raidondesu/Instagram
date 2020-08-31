f47b7078ca4c2055d75a9b2f29427392a0e4f808
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-31 13:26:52 -0400

    Material UI dependency

--- instagram/src/Post.js
+++ instagram/src/Post.js
@@ -10,6 +10,10 @@ function Post() {
         className="post__image"
         src="https://venturebeat.com/wp-content/uploads/2015/01/Microsoft-sign-campus.jpg?fit=4160%2C2340&strip=all"
       />
+      <h4 className="post__text">
+        <strong>Microsoft</strong>
+      </h4>
+      <p>Redmond Campus!</p>
       {/* image */} {/* username + caption */}
     </div>
   );
