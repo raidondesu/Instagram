d61c68bef0b82758c89fe6eaf0a087ad8faf6c49
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-08-31 10:08:24 -0400

    Instagram logo and basic styling for header

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,8 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+          alt=""
+        />
+      </div>
       {/* Posts */}
       {/* Posts */}
     </div>
