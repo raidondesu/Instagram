860d51d2f8112350cb8b454206a432ab3e1f5a7a
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-02 14:45:51 -0400

    Foundation react hook for managing state of INstagram posts

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -17,7 +19,11 @@ function App() {
         caption="Seattle !"
         imageUrl="https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/06/20/104540684-GettyImages-530874379.1910x1000.jpg"
       />
-      <Post username="Microsoft" caption="Seattle !" imageUrl="" />
+      <Post
+        username="Microsoft"
+        caption="Redmond Campus !"
+        imageUrl="https://assets.pcmag.com/media/images/564770-microsoft-redmond-campus-expansion.jpg?thumb=y&width=810&height=456"
+      />
       <Post username="Raidondesu" caption="Seattle !" imageUrl="" />
       {/* Posts */}
       {/* Posts */}
