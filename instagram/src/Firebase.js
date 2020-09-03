d413a9e11d79b8ad116c37ec2e40851994a4fcb4
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-03 12:15:34 -0400

    Imported and Initialize firebase / export database, authentication and storage constants

--- instagram/src/Firebase.js
+++ instagram/src/Firebase.js
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyB835sJnt4gGRHvAhPFGUfNyyS9oo-29mA",
   authDomain: "instagram-2a7e4.firebaseapp.com",
   databaseURL: "https://instagram-2a7e4.firebaseio.com",
@@ -7,4 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "669906632292",
   appId: "1:669906632292:web:193fb7bcefb04451ca6971",
   measurementId: "G-JFBG5RDMRV",
-};
+});
+
+const db = firebaseApp.fireStore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
