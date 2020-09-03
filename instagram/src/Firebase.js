eb452c9a4641bceaec2752c97e10e1c11116974d
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-03 12:13:12 -0400

    Created Firebase.js
    Imported database config

---
+++ instagram/src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB835sJnt4gGRHvAhPFGUfNyyS9oo-29mA",
+  authDomain: "instagram-2a7e4.firebaseapp.com",
+  databaseURL: "https://instagram-2a7e4.firebaseio.com",
+  projectId: "instagram-2a7e4",
+  storageBucket: "instagram-2a7e4.appspot.com",
+  messagingSenderId: "669906632292",
+  appId: "1:669906632292:web:193fb7bcefb04451ca6971",
+  measurementId: "G-JFBG5RDMRV",
+};
