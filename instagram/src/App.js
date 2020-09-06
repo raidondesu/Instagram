08442e0704136c92dc791c6d59a519f3d5d764d2
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-06 02:56:56 -0400

    Foundation for Material UI React Modal Component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,10 +1,41 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./Firebase";
 
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   // useEffect runs code based on a specific condition
   useEffect(() => {
@@ -20,6 +51,11 @@ function App() {
   }, []);
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           className="app__headerImage"
