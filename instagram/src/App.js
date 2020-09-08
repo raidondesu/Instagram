1082b4b690f7b3daf2a3ecedc95ab1087e646fef
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-07 23:50:03 -0400

    Typo (Sign in was sign up)

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -171,7 +171,7 @@ function App() {
         <Button onClick={() => auth.signOut()}>Logout</Button>
       ) : (
         <div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign Up</Button>
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
           <Button onClick={() => setOpen(true)}>Sign Up</Button>
         </div>
       )}
