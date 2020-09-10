c9e3ed32f4bcf1018e125dcd0549ff768dbd5e2e
Raidon <48734335+raidondesu@users.noreply.github.com>
2020-09-09 23:59:22 -0400

    react hook and input foundation

--- instagram/src/ImageUpload.js
+++ instagram/src/ImageUpload.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function ImageUpload() {
+  const [caption, setCaption] = useState('');
   return (
     <div>
       {/* Caption */}
       {/* File picker */}
       {/* Post Button */}
+      <input type="text" placeholder='Enter a caption..' onChange={event => setCaption(event.target.value) value=""}
     </div>
   );
 }
